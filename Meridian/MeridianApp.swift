9d0a752ff5ec23498c4d6fd2df6d77fe57e96a98
Franco Leto <letoefranco@gmail.com>
2026-01-10 11:14:43 -0300

    Initial Commit

---
+++ Meridian/MeridianApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeridianApp.swift
+//  Meridian
+//
+//  Created by Franco Ezequiel Leto on 10/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MeridianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeridianTests/MeridianTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeridianTests.swift
+//  MeridianTests
+//
+//  Created by Franco Ezequiel Leto on 10/01/2026.
+//
+
+import Testing
+@testable import Meridian
+
+struct MeridianTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
